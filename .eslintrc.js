115c1ff9624b0d196bfda7e3160734a18c3c72b9
Bilo Lwabona <bilo.lwabona@gmail.com>
2021-03-09 10:01:58 +0200

    added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        es6: true
     },
-    "extends": "standard",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: 'standard',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
     },
-    "rules": {
+    rules: {
+        indent: ['error', 4],
+        'no-unused-vars': 0
     }
-};
+}
