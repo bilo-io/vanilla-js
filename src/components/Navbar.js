4fe80d24cc2dd87194712bab9c8c4e8eba20249e
Bilo Lwabona <bilo.lwabona@gmail.com>
2021-03-10 02:40:04 +0200

    cleaned code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,63 @@
 import Vision, { on, html } from '../core/vision'
 
-// COMPONENT
+let state = { isOpen: false }
+const navItems = [
+    {
+        name: 'Home',
+        path: '/home'
+    },
+    {
+        name: 'Profile',
+        path: '/profile'
+    },
+    {
+        name: 'Log out',
+        path: '/login'
+    }
+]
+
+const setState = (newState, callback = () => {}) => {
+    state = { ...state, isOpen: !state.isOpen }
+    window.dispatchEvent(new Event('setState', {}))
+}
 export const Navbar = async ({ title }) => new Vision({
     isGlobal: true,
+    setState,
     view: async () => html`
+    <div>
         <div id="Navbar" class="flex-row">
-            <button style="padding: 0 1rem 0 1rem; color: inherit">
+            <button class="menu-toggle" style="padding: 0 1rem 0 1rem; color: inherit">
                 <i class="fas fa-bars" style="color: inherit"></i>
             </button>
             <div style="margin-left: 1rem">${title}</div>
         </div>
+        <div id="NavMenu" class="${state.isOpen ? 'open' : ''}">
+            ${[].map(item => (
+        html`<button class="nav-item" name="nav-item-${item.path.replace('/', '')}">${item.name}</button>`
+    ))}
+        </div>
+    </div>
     `,
     controls: async () => {
         const container = await document.getElementById('Navbar')
+        const navMenu = await document.getElementById('NavMenu')
 
         console.log('Navbar.container', container)
 
-        on('click', container, 'button',
+        on('click', container, 'button.menu-toggle',
             () => {
                 console.log('clicked button')
-                alert('Yo dude')
+                // state =
+                setState({ ...state, isOpen: !state.isOpen })
+                console.log('state', state)
             })
+
+        // navItems.forEach(item => {
+        //     on('click', navMenu, `button.nav-item-${item.path.replace('/', '')}`,
+        //         () => {
+        //             console.log('NavItem.onClick', item)
+        //         })
+        // })
     }
 })
 
